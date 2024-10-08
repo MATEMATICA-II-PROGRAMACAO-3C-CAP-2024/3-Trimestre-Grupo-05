227458640d9dde42460b06117952f0d48ac98512
EnzoYK <128195019+EnzoYK@users.noreply.github.com>
2024-10-08 09:39:52 -0300

    3_Trimestre_Grupo_05

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
-        alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "Insira o enunciado da Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
     },
-
     {
-        enunciado: "Pergunta 2",
-        alternativas: ["Alternativa 1", "Alternativa 2"],
-    },  
-]
+    enunciado: "Insira o enunciado da Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+    },
+    {
+    enunciado: "Insira o enunciado da Pergunta 3",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+    }
+];
 
 const lapis = {
     tamanho: 20,
@@ -21,3 +24,22 @@ const lapis = {
     cor: 'Grafite',
     temBorrachaAtras: false
 }
+
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+}
+
+const caixa-perguntas = document.querySelector
+(".caixa-perguntas");
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent =
+}
+
+function mostraPergunta() {
+        perguntaAtual = perguntas[atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
+        }
+        mostraPergunta();
